be379067bce9137a59a0f04278e09b721e7ead9a
Elyas Page <elyaspage@googlemail.com>
2020-11-16 21:25:42 +0000

    Created app directory, moved bin, public, routes, views into app

--- app.js
+++ app.js
@@ -12,21 +12,21 @@
  const logger = require('morgan');
 
  /* --- Routers --- */
- const indexRouter = require('./routes/index');
- const usersRouter = require('./routes/users');
+ const indexRouter = require('./routes/app/index');
+ const usersRouter = require('./routes/app/users');
  /* --------------- */
 
  const app = express();
 
  // view engine setup
- app.set('views', path.join(__dirname, 'views'));
+ app.set('views', path.join(__dirname, 'app', 'views'));
  app.set('view engine', 'pug');
 
  app.use(logger('dev'));
  app.use(express.json());
  app.use(express.urlencoded({ extended: false }));
  app.use(cookieParser());
- app.use(express.static(path.join(__dirname, 'public')));
+ app.use(express.static(path.join(__dirname, 'app', 'public')));
 
  app.use('/', indexRouter);
  app.use('/users', usersRouter);
