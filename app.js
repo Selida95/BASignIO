35fcdca0ade3db19a9b4a4a2cf7b9ea506b05a46
Elyas Page <elyaspage@googlemail.com>
2020-11-17 21:07:20 +0000

    Added database connection

--- app.js
+++ app.js
@@ -11,12 +11,24 @@
  const cookieParser = require('cookie-parser');
  const logger = require('morgan');
  const mongoose = require('mongoose');
+ const crypto = require('crypto');
 
  /* --- Config --- */
  const config = require('./app/config');
  /* -------------- */
 
+ /* --- Database --- */
+ mongoose.Promise = global.Promise;
+ mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name, {
+   useMongoClient: true
+ })
 
+ var db = mongoose.connection;
+ db.on('error', console.error.bind(console, 'connection error:'));
+ db.once('open', () => {
+   console.log('Connected to Database...');
+ });
+ /* ---------------- */
 
  /* --- Routers --- */
  const indexRouter = require('./routes/app/index');
