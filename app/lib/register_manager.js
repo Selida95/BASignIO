d88e8c0f3113286cba6f4659795e8e295a81b01d
Elyas Page <elyaspage@googlemail.com>
2020-12-01 20:53:17 +0000

    Created register manager. Added createRecord method.

---
+++ app/lib/register_manager.js
@@ -0,0 +1,65 @@
+/*
+ * ----------------------------
+ * BASignIO - Register Manager
+ * ----------------------------
+ */
+
+ // Dependencies
+ const util = require('./utilities')
+
+ // Database Models
+
+
+ // Config
+ const config = require('../config');
+
+ // Define manager object
+ var manager = {};
+
+ // Create Record
+ // Required Fields: parameterObject (contains: id, forenames, surname, type, location, IO state), callback
+ // Optional Fields: yearGroup (dependent if student or staff)
+ manager.createRecord = (parameterObject, callback) => {
+   let ObjectID = require('mongodb').ObjectID;
+
+   // Validate required fields
+   let id = typeof(parameterObject.id) === 'string' && parameterObject.id.length > 0 ? parameterObject.id : false;
+   let forenames = typeof(parameterObject.forenames) === 'string' && parameterObject.forenames.length > 0  ? parameterObject.forenames : false
+   let surname = typeof(parameterObject.surname) === 'string' && parameterObject.surname.length > 0  ? parameterObject.surname : false
+   let type = typeof(parameterObject.type) === 'string' && parameterObject.type.toLowerCase() == 'student' || parameterObject.type.toLowerCase() == 'staff' ? parameterObject.type.toLowerCase() : false
+   let location = typeof(parameterObject.location) === 'string' && parameterObject.location.length > 0  ? parameterObject.location : false
+   let io = typeof(parseInt(parameterObject.io)) === 'number' && parseInt(parameterObject.io) == 0 || parseInt(parameterObject.io) == 1 ? parseInt(parameterObject.io) : false
+
+   // Validate optional field
+   let yearGroup = typeof(parseInt(parameterObject.yearGroup)) === 'number' && parseInt(parameterObject.yearGroup) < 14 && parseInt(parameterObject.yearGroup) > 0 && type === 'student' ? parseInt(parameterObject.yearGroup) : undefined
+
+   if (id && forenames && surname && type && location && io) {
+     let Register = new register({
+       _id : new ObjectID(),
+       id : id,
+       surname : surname,
+       forenames : forenames,
+       type : type,
+       yearGroup : yearGroup,
+       loc : location.toUpperCase(),
+       timeIn : io === 1 ? utils.time() : '',
+       timeOut : io === 0 ? utils.time() : '',
+       io : io,
+       date : utils.date()
+     },
+     {
+       collection : 'registers',
+       versionKey : false
+     })
+
+     Register.save((error, record) => {
+       if (error) throw error;
+       callback({ message : 'SUCCESS', data : record })
+     })
+   } else {
+     throw new Error('MISSING_REQUIRED_FIELDS')
+   }
+ }
+
+ // Export module
+ module.exports = manager
