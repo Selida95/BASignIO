407c47603ba805fb12226e7c658823ab696c0c93
Elyas Page <elyaspage@googlemail.com>
2020-12-02 20:35:58 +0000

    Added updateRecord method to fire register manager

--- app/lib/fire_register_manager.js
+++ app/lib/fire_register_manager.js
@@ -32,5 +32,33 @@
    })
  }
 
+ // Update Record
+ // Required Fields: id, io (in or out) state, location, callback
+ // Optional Fields: none
+ manager.updateRecord = (parameterObject, callback) => {
+   // Validate required fields
+   let id = typeof(parameterObject.id) === 'string' && parameterObject.id.length > 0 ? parameterObject.id : false;
+   let location = typeof(parameterObject.location) === 'string' && parameterObject.location.length > 0  ? parameterObject.location : false
+   let io = typeof(parseInt(parameterObject.io)) === 'number' && parseInt(parameterObject.io) == 0 || parseInt(parameterObject.io) == 1 ? parseInt(parameterObject.io) : false
+
+   if (id && location && io) {
+     manager.getRecord(id, (record) => {
+       if (record.message === 'NOT_FOUND') throw new Error('NOT_FOUND')
+
+       record.data.timeIn = io === 1 ? util.time() : record.data.timeIn
+       record.data.timeOut = io === 0 ? util.time() : record.data.timeOut
+       record.data.date = util.date()
+       record.data.io = io
+       record.data.location = location.toUpperCase()
+
+       record.data.save()
+
+       callback({ message : 'SUCCESS' })
+     })
+   } else {
+     throw new Error('MISSING_REQUIRED_FIELDS')
+   }
+ }
+
  // Export module
  module.exports = manager;
