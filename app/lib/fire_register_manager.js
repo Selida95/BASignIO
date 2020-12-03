f428a97f7903d9e8a4ef2ad7fd5829a0b6169299
Elyas Page <elyaspage@googlemail.com>
2020-12-03 20:37:09 +0000

    Added createRecord method for fire register manager

--- app/lib/fire_register_manager.js
+++ app/lib/fire_register_manager.js
@@ -17,6 +17,50 @@
  // Define manager object
  var manager = {}
 
+ // Create Record
+ // Required Fields:
+ // Optional Fields:
+ manager.createRecord = (parameterObject, callback) => {
+   // Validate required fields
+   let id = typeof(parameterObject.id) === 'string' && parameterObject.id.length > 0 ? parameterObject.id : false;
+   let forenames = typeof(parameterObject.forenames) === 'string' && parameterObject.forenames.length > 0  ? parameterObject.forenames : false
+   let surname = typeof(parameterObject.surname) === 'string' && parameterObject.surname.length > 0  ? parameterObject.surname : false
+   let type = typeof(parameterObject.type) === 'string' && parameterObject.type.toLowerCase() == 'student' || parameterObject.type.toLowerCase() == 'staff' ? parameterObject.type.toLowerCase() : false
+   let location = typeof(parameterObject.location) === 'string' && parameterObject.location.length > 0  ? parameterObject.location : false
+   let io = typeof(parseInt(parameterObject.io)) === 'number' && parseInt(parameterObject.io) == 0 || parseInt(parameterObject.io) == 1 ? parseInt(parameterObject.io) : false
+
+   // Validate optional field
+   let yearGroup = typeof(parseInt(parameterObject.yearGroup)) === 'number' && parseInt(parameterObject.yearGroup) < 14 && parseInt(parameterObject.yearGroup) > 0 && type === 'student' ? parseInt(parameterObject.yearGroup) : undefined
+   let tutorGrp = typeof(parameterObject.tutorGrp) === 'string' && parameterObject.tutorGrp.length > 0  ? parameterObject.tutorGrp : undefined
+
+   if (id && forenames && surname && type && location && io) {
+     let Register = new fire_register({
+       _id: id,
+       surname: surname,
+       forenames: forenames,
+       yearGroup: yearGroup,
+       tutorGrp: tutorGrp,
+       type: type,
+       loc: location.toUpperCase(),
+       timeIn: io === 1 ? utils.time() : '',
+       timeOut: io === 0 ? utils.time() : '',
+       io: io,
+       date: utils.date()
+     },
+     {
+       collection: 'fireRegisters',
+       versionKey: false
+     });
+
+     Register.save((err, records) => {
+       if (error) throw error;
+       callback({ message : 'SUCCESS', data : record })
+     })
+   } else {
+     throw new Error('MISSING_REQUIRED_FIELDS')
+   }
+ }
+
  // Get Record
  // Required Fields: id, callback
  // Optional Fields: none
@@ -35,7 +79,7 @@
  // Update Record
  // Required Fields: id, io (in or out) state, location, callback
  // Optional Fields: none
- manager.updateRecord = (parameterObject, callback) => {
+ manager.updateRecord = (parameterObject) => {
    // Validate required fields
    let id = typeof(parameterObject.id) === 'string' && parameterObject.id.length > 0 ? parameterObject.id : false;
    let location = typeof(parameterObject.location) === 'string' && parameterObject.location.length > 0  ? parameterObject.location : false
@@ -52,8 +96,6 @@
        record.data.location = location.toUpperCase()
 
        record.data.save()
-
-       callback({ message : 'SUCCESS' })
      })
    } else {
      throw new Error('MISSING_REQUIRED_FIELDS')
