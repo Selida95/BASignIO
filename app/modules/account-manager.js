4bbb5ce99ccbe491951965040d49e5c770c7fb50
Elyas Page <elyaspage@googlemail.com>
2020-12-11 10:54:26 +0000

    Reformated account-manager. Added createNewUser method

--- app/modules/account-manager.js
+++ app/modules/account-manager.js
@@ -1,8 +1,58 @@
-const crypto = require('crypto');
+/*
+ * --------------------------
+ * BASignIO: Account Manager
+ * --------------------------
+ */
 
-var account = require('../models/accounts.js');
+ // Dependencies
+ const crypto = require('crypto');
 
-const config = require('../config')
+ // Database Models
+ const accountModel = require('../models/accounts.js');
+
+ // Config
+ const config = require('../config')
+
+ // Define Manager Object
+ let manager = {}
+
+ // Create New User
+ // Required Fields: parameterObject (contains: username, email, password), callback
+ // Optional Fields: parameterObject (contains: firstName, lastName, role)
+ manager.createNewUser = (parameterObject, callback) => {
+	 let username = typeof(parameterObject.username) === 'string' && parameterObject.username.length > 0 ? parameterObject.username.trim() : false;
+	 let email = typeof(parameterObject.email) === 'string' && parameterObject.email.length > 0 ? parameterObject.email.trim() : false;
+	 let password = typeof(parameterObject.password) === 'string' && parameterObject.password.length > 0 ? parameterObject.password.trim() : false;
+
+	 if (username && email && password) {
+		 let account = new accountModel({
+			 username : username,
+			 email : email,
+			 password : manager.hash(password),
+			 firstName : typeof(parameterObject.firstName) === 'string' && parameterObject.firstName.length > 0 ? parameterObject.firstName.trim() : '',
+			 lastName : typeof(parameterObject.lastName) === 'string' && parameterObject.lastName.length > 0 ? parameterObject.lastName.trim() : '',
+			 role : typeof(parameterObject.role) === 'string' && parameterObject.role.length > 0 && parameterObject.role.toLowerCase() === 'admin' ? 'admin' : 'user'
+		 })
+
+		 account.save((error) => {
+			 if (error) throw error;
+
+			 callback({ message : 'SUCCESS' });
+		 })
+	 } else {
+		 throw new Error('REQUIRED_FIELD_INVALID')
+	 }
+ }
+
+ // Get Users
+ // Required Fields: id, callback
+ // Optional Fields: None
+ manager.getUser = (id, callback) => {
+	 
+ }
+
+ // Export module
+ module.exports = manager;
 
 // --- Exports --- //
 
