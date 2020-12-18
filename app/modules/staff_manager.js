5cde18a3f7dde0a6901505b5ab2c789da3b9421a
Elyas Page <elyaspage@googlemail.com>
2020-12-18 14:17:14 +0000

    Init commit for staff manager

---
+++ app/modules/staff_manager.js
@@ -0,0 +1,19 @@
+/*
+ * ------------------------
+ * BASignIO: Staff Manager
+ * ------------------------
+ */
+
+ // Dependencies
+
+
+ // Database models
+ const staffModel = require('../models/staff')
+
+ // Define manager object
+ let manager = {}
+
+ 
+
+ // Export Module
+ module.exports = manager;
