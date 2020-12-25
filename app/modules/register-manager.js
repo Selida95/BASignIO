2207d6955cec9b190dcd368165baf2332669f048
Elyas Page <elyaspage@googlemail.com>
2020-12-25 00:11:27 +0000

    Added timeIn and timeOut as optional fields for both createNewRecords and updateRecord

--- app/modules/register-manager.js
+++ app/modules/register-manager.js
@@ -16,7 +16,7 @@
 
  // Create New Record
  // Required Fields: parameterObject(contains: id, surname, forenames, type, loc, io), callback
- // Optional Fields: parameterObject(contains: yearGroup, tutorGrp)
+ // Optional Fields: parameterObject(contains: yearGroup, tutorGrp, timeIn, timeOut)
  manager.createNewRecord = (parameterObject, callback) => {
    let id = typeof(parseInt(parameterObject.id)) === 'number' && !isNaN(parameterObject.id) ? parseInt(parameterObject.id) : false
    let forenames = typeof(parameterObject.forenames) === 'string' && parameterObject.forenames.length > 0 ? parameterObject.forenames : false
@@ -24,6 +24,8 @@
    let type = typeof(parameterObject.type) === 'string' && parameterObject.type.length > 0 ? parameterObject.type : false
    let location = typeof(parameterObject.loc) === 'string' && parameterObject.loc.length > 0 ? parameterObject.loc : false
    let io = typeof(parseInt(parameterObject.io)) === 'number' && !isNaN(parameterObject.io) ? parseInt(parameterObject.io) : false
+   let timeIn = typeof(parameterObject.timeIn) === 'string' && parameterObject.timeIn.length > 0 ? parameterObject.timeIn : false
+   let timeOut = typeof(parameterObject.timeOut) === 'string' && parameterObject.timeOut.length > 0 ? parameterObject.timeOut : false
 
    if (id && forenames && surname && type && location && io) {
      let register = new registerModel({
@@ -36,8 +38,8 @@
        io : io,
        yearGroup : type === 'student' && typeof(parseInt(parameterObject.yearGroup)) === 'number' && isNaN(parameterObject.yearGroup) ? parseInt(parameterObject.yearGroup) : undefined,
        tutorGrp : type === 'student' && typeof(parameterObject.tutorGrp) === 'string' && parameterObject.tutorGrp.length > 0 ? parameterObject.tutorGrp : undefined,
-       timeIn : io === 1 ? utils.time() : 'N/A',
-       timeOut : io === 0 ? utils.time() : '',
+       timeIn : timeIn ? timeIn : '',
+       timeOut : timeOut ? timeOut : '',
        date : utils.date()
      }, {
        collection: 'registers',
@@ -78,7 +80,7 @@
  // Get All Records
  // Required Fields: parameterObject(If not optional fields, empty object), callback
  // Optional Fields: parameterObject(contains: date)
- manager.getAllRecords = (callback) => {
+ manager.getAllRecords = (parameterObject, callback) => {
    let query = {}
    query.date = typeof(parameterObject.date) === 'string' && parameterObject.date.length > 0 ? parameterObject.date : false
 
@@ -98,18 +100,20 @@
 
  // Update Record
  // Required Fields: parameterObject(contains: id, io), callback
- // Optional Fields: none
+ // Optional Fields: parameterObject(contains: timeIn, timeOut)
  manager.updateRecord = (parameterObject, callback) => {
    let id = typeof(parseInt(parameterObject.id)) === 'number' && !isNaN(parameterObject.id) ? parseInt(parameterObject.id) : false
    let io = typeof(parseInt(parameterObject.io)) === 'number' && !isNaN(parameterObject.io) ? parseInt(parameterObject.io) : false
+   let timeIn = typeof(parameterObject.timeIn) === 'string' && parameterObject.timeIn.length > 0 ? parameterObject.timeIn : false
+   let timeOut = typeof(parameterObject.timeOut) === 'string' && parameterObject.timeOut.length > 0 ? parameterObject.timeOut : false
 
    if (typeof(id) === 'number' && typeof(io) === 'number') {
      manager.getLatestRecord({
        id : id
      }, (record) => {
        record.data.io = io
-       record.data.timeIn = record.data.io === io && io === 0 || record.data.date !== utils.date() ? 'N/A' : utils.time()
-       record.data.timeOut = record.data.io === io && io === 1 || record.data.date !== utils.date() ? 'N/A' : utils.time()
+       record.data.timeIn = timeIn ? timeIn : record.data.timeIn
+       record.data.timeOut = timeOut ? timeOut : record.data.timeOut
        record.data.save((error) => {
          if (error) throw error
 
